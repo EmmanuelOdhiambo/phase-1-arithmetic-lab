f708c640eb5d90d31cfa974eb8ab339ec4f849a5
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-18 07:02:35 +0300

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 8;
+const num2 = 7.75;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100);
+
+const num3 = 4;
+const num4 = 15.5;
+const mod = num3 % num4;
+
+const max = Math.max(2,5,7,20,8);
+
+
+
+ 
+
+    
+
+
